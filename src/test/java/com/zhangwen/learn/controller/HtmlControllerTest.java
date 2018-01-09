ca8635c8715c5d5598b10dec8a2fc432039e701b
gudanrenao <zhangwenit@126.com>
2018-01-09 16:54:45 +0800

    gitignore

---
+++ src/test/java/com/zhangwen/learn/controller/HtmlControllerTest.java
@@ -0,0 +1,12 @@
+package com.zhangwen.learn.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HtmlControllerTest {
+
+    @Test
+    public void index() {
+    }
+}
